72a8b5594d0afd558175d4432ac3aa77092e4b94
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-09-08 18:00:27 +0200

    Add test to check that examples run without errors

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,17 +2,28 @@ import subprocess
 
 import pytest
 
-EXAMPLES = [
+EXAMPLE_MD = [
     "README.md",
     "docs/index.md",
 ]
 
+EXAMPLE_SCRIPTS = [
+    "examples/estimation_methods.py",
+    "examples/batch_and_online_estimation.py",
+]
 
-@pytest.mark.parametrize("filepath", EXAMPLES)
-def test_examples(filepath):
+
+@pytest.mark.parametrize("filepath", EXAMPLE_MD)
+def test_examples_in_md(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
         file = f.read()
 
     python_code = file.split("```python")[1].split("```")[0]
     proc = subprocess.run(["python", "-c", python_code])
     assert proc.returncode == 0, "The extracted code did not run successfully"
+
+
+@pytest.mark.parametrize("script", EXAMPLE_SCRIPTS)
+def test_examples_in_py(script):
+    proc = subprocess.run(["python", script])
+    assert proc.returncode == 0, f"The example script {script} did not run successfully"
