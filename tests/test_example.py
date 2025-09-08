f72e58a2c2675ec2ee7759f6e0c8b550d80ed0d4
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-09-08 18:57:37 +0200

    Use exec instead of subprocess

--- tests/test_example.py
+++ tests/test_example.py
@@ -21,11 +21,13 @@ def test_examples_in_md(filepath):
         file = f.read()
 
     python_code = file.split("```python")[1].split("```")[0]
-    proc = subprocess.run([sys.executable, "-c", python_code], env=os.environ.copy())
-    assert proc.returncode == 0, "The extracted code did not run successfully"
+    exec(python_code, {}, {})
+    # proc = subprocess.run([sys.executable, "-c", python_code], env=os.environ.copy())
+    # assert proc.returncode == 0, "The extracted code did not run successfully"
 
 
 @pytest.mark.parametrize("script", EXAMPLE_SCRIPTS)
 def test_examples_in_py(script):
-    proc = subprocess.run([sys.executable, script], env=os.environ.copy())
-    assert proc.returncode == 0, f"The example script {script} did not run successfully"
+    exec(open(script).read(), {}, {})
+    # proc = subprocess.run([sys.executable, script], env=os.environ.copy())
+    # assert proc.returncode == 0, f"The example script {script} did not run successfully"
