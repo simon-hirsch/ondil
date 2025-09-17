Fix sign

src/ondil/estimators/online_mvdistreg.py
@@ -1364,7 +1364,7 @@ class MultivariateOnlineDistributionalRegressionPath(
         out = {}
         for a in range(self.adr_steps_):
             out[a] = {}
-            if a >= self.optimal_adr_:
+            if a <= self.optimal_adr_:
                 for p in range(self.distribution.n_params):
                     array = np.zeros((N, self.n_dist_elements_[p]))
                     for k in range(self.n_dist_elements_[p]):