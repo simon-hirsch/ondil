932482e20958389e4b98153b647b43960e5e03bc
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-10-02 16:13:04 +0200

    Fix API URL

--- .github/scripts/contributors.py
+++ .github/scripts/contributors.py
@@ -46,7 +46,7 @@ def fetch_contributors():
 
     issues_data = []
     for label in labels:
-        issues_url = f"https://api.github.com/repos/{OWNER}/{REPO}/issues?state=closed&labels={label}&per_page=100"
+        issues_url = f"https://api.github.com/repos/{OWNER}/{REPO}/issues?state=closed&label={label}&per_page=100"
         issues_data += paginated_get(issues_url)
 
     issuers = {
