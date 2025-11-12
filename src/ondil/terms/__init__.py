4663f9f80762df253805dc067f68d30a2427aab5
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-11-12 16:11:03 +0100

    Simple term that adds a scikit-learn estimator as a term in the model.

--- src/ondil/terms/__init__.py
+++ src/ondil/terms/__init__.py
@@ -1,6 +1,8 @@
 from .linear_terms import LinearTerm, RegularizedLinearTermIC
+from .special import ScikitLearnEstimatorTerm
 
 __all__ = [
     "LinearTerm",
     "RegularizedLinearTermIC",
+    "ScikitLearnEstimatorTerm",
 ]
