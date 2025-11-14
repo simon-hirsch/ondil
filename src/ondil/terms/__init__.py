d462918d10f1363a2575f8ba0cf528845343733a
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-11-14 13:13:03 +0100

    Add autoregressive term to terms __init__.py

--- src/ondil/terms/__init__.py
+++ src/ondil/terms/__init__.py
@@ -1,7 +1,9 @@
 from .linear_terms import LinearTerm, RegularizedLinearTermIC
 from .special import ScikitLearnEstimatorTerm
+from .autoregressive_terms import AutoregressiveTerm
 
 __all__ = [
+    "AutoregressiveTerm",
     "LinearTerm",
     "RegularizedLinearTermIC",
     "ScikitLearnEstimatorTerm",
