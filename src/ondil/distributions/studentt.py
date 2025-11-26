fcc5c4fe59db1232baa513b9cc94ade424abd3ee
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-11-26 15:19:45 +0100

    Non-constant start values for student-t

--- src/ondil/distributions/studentt.py
+++ src/ondil/distributions/studentt.py
@@ -36,6 +36,7 @@ class StudentT(ScipyMixin, Distribution):
         loc_link: LinkFunction = Identity(),
         scale_link: LinkFunction = Log(),
         tail_link: LinkFunction = LogShiftTwo(),
+        start_value_optimism: float = 0.0,
     ) -> None:
         super().__init__(
             links={
@@ -44,6 +45,7 @@ class StudentT(ScipyMixin, Distribution):
                 2: tail_link,
             }
         )
+        self.start_value_optimism = start_value_optimism
 
     def dl1_dp1(self, y: np.ndarray, theta: np.ndarray, param: int = 0) -> np.ndarray:
         self._validate_dln_dpn_inputs(y, theta, param)
@@ -115,6 +117,17 @@ class StudentT(ScipyMixin, Distribution):
             _, sigma, nu = self.theta_to_params(theta)
             return 2 / (sigma * (nu + 3) * (nu + 1))
 
-    def initial_values(self, y: np.ndarray) -> np.ndarray:
+    def constant_initial_values(self, y: np.ndarray) -> np.ndarray:
         params = st.t.fit(y)
         return np.tile([params[1], params[2], params[0]], (y.shape[0], 1))
+
+    def initial_values(self, y: np.ndarray) -> np.ndarray:
+        constant_values = self.constant_initial_values(y=y)
+        dynamic_values = np.zeros_like(constant_values)
+        dynamic_values[:, 0] = y  # mu
+        dynamic_values[:, 1] = np.abs(y - np.mean(y))  # sigma
+        dynamic_values[:, 2] = np.abs(y - np.mean(y)) / np.std(y)  # nu
+        return (
+            self.start_value_optimism * constant_values
+            + (1 - self.start_value_optimism) * dynamic_values
+        )
