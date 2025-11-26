dd040b7a6e2588722171284eea7216b7f9dadf60
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-11-26 10:44:23 +0100

    Add joint estimation term

--- src/ondil/terms/__init__.py
+++ src/ondil/terms/__init__.py
@@ -2,11 +2,13 @@ from .autoregressive_terms import (
     AutoregressiveSquaredResidualTerm,
     AutoregressiveTargetTerm,
     AutoregressiveThetaTerm,
+    JointEstimationTimeSeriesTerm,
 )
 from .linear_terms import InterceptTerm, LinearTerm, RegularizedLinearTermIC
 from .special import ScikitLearnEstimatorTerm
 
 __all__ = [
+    "JointEstimationTimeSeriesTerm",
     "InterceptTerm",
     "AutoregressiveThetaTerm",
     "AutoregressiveTargetTerm",
