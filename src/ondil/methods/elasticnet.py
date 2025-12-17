999cc5472c36121025912f24f3e84759176e551d
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-12-17 17:28:28 +0100

    Allow for kwargs in Method.fit() and method.update()

--- src/ondil/methods/elasticnet.py
+++ src/ondil/methods/elasticnet.py
@@ -127,7 +127,7 @@ class ElasticNetPath(EstimationMethod):
     def update_y_gram(gram, X, y, weights, forget):
         return update_y_gram(gram, X, y, forget=forget, w=weights)
 
-    def fit_beta_path(self, x_gram, y_gram, is_regularized):
+    def fit_beta_path(self, x_gram, y_gram, is_regularized, **kwargs):
         self._validate_bounds(x_gram=x_gram)
         lambda_max = self._get_lambda_max(
             x_gram=x_gram, y_gram=y_gram, is_regularized=is_regularized
@@ -135,6 +135,8 @@ class ElasticNetPath(EstimationMethod):
         lambda_path = np.geomspace(
             lambda_max, lambda_max * self.lambda_eps, self.lambda_n
         )
+        regularization_weights = kwargs.get("regularization_weights", None)
+
         beta_path = np.zeros((self.lambda_n, x_gram.shape[0]))
         beta_path, _ = online_coordinate_descent_path(
             x_gram=x_gram,
@@ -147,14 +149,18 @@ class ElasticNetPath(EstimationMethod):
             beta_lower_bound=self.beta_lower_bound,
             beta_upper_bound=self.beta_upper_bound,
             which_start_value=self.start_value_initial,
-            regularization_weights=self.regularization_weights,
+            regularization_weights=regularization_weights,
             selection=self.selection,
             tolerance=self.tolerance,
             max_iterations=self.max_iterations,
         )
         return beta_path
 
-    def update_beta_path(self, x_gram, y_gram, beta_path, is_regularized):
+    def update_beta_path(self, x_gram, y_gram, beta_path, is_regularized, **kwargs):
+        self._validate_bounds(x_gram=x_gram)
+
+        regularization_weights = kwargs.get("regularization_weights", None)
+
         lambda_max = self._get_lambda_max(
             x_gram=x_gram, y_gram=y_gram, is_regularized=is_regularized
         )
@@ -172,15 +178,15 @@ class ElasticNetPath(EstimationMethod):
             beta_lower_bound=self.beta_lower_bound,
             beta_upper_bound=self.beta_upper_bound,
             which_start_value=self.start_value_update,
-            regularization_weights=self.regularization_weights,
+            regularization_weights=regularization_weights,
             selection=self.selection,
             tolerance=self.tolerance,
             max_iterations=self.max_iterations,
         )
         return beta_path
 
-    def fit_beta(self, x_gram, y_gram, is_regularized):
+    def fit_beta(self, x_gram, y_gram, **kwargs):
         pass
 
-    def update_beta(self, x_gram, y_gram, is_regularized):
+    def update_beta(self, x_gram, y_gram, **kwargs):
         pass
