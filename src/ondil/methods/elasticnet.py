Fix calculation of number of non-zero coefficients (#189)
* Fix calculation of number of non-zero coefficients

* Use += consistently and adjust comment

* Ensure lambda_max cannot be 0

* Initialize with correct length

---------

Co-authored-by: Jonathan Berrisch <jonathan@berrisch.biz>

src/ondil/methods/elasticnet.py
@@ -123,7 +123,11 @@ class ElasticNetPath(EstimationMethod):
                 weights[is_regularized]
             )
 
-        return np.max(abs_gram)
+        lambda_max = np.max(abs_gram)
+        if np.isclose(lambda_max, 0.0):
+            lambda_max = 1.0
+
+        return lambda_max
 
     def _validate_bounds(self, x_gram: np.ndarray) -> None:
         J = x_gram.shape[1]